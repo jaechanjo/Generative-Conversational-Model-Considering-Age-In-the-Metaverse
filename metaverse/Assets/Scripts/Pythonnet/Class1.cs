87def5244d60c75344c0da1dca2f9dffb78d65cc
youngyoon-lim <117579307+Lim-YoungYoon@users.noreply.github.com>
2022-12-13 08:05:22 +0900

    back

---
+++ metaverse/Assets/Scripts/Pythonnet/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Pythonnet
+{
+    public class Class1
+    {
+        public int c;
+        public void AddValues(int a, int b)
+        {
+            c = a + b;
+        }
+
+        public void Test()
+        {
+            Debug.Log("DLL에서 로드 했습니다.");
+        }
+
+        public static int GenerationRandom(int min, int max)
+        {
+            System.Random rand = new System.Random();
+            return rand.Next(min, max);
+        }
+    }
+}
