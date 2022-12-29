da2af5a4f3e5db99cb12ae4f0f35a9e8c11b49d7
youngyoon-lim <117579307+Lim-YoungYoon@users.noreply.github.com>
2022-12-29 10:38:18 +0900

    edit

--- MetaVH/Assets/Scripts/pythonnet.cs
+++ MetaVH/Assets/Scripts/pythonnet.cs
@@ -23,6 +23,11 @@ public class pythonnet : MonoBehaviour
     private AudioSource Audio;
     public GameObject GameObject;
 
+    public string char_input;
+    public string char_output;
+
+    public int result = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -40,15 +45,14 @@ public class pythonnet : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Z))
         {
             Run();
-            Talk();
         }
-
     }
 
     async void Run()
     {
-       await Task.Run(() =>
+        await Task.Run(() =>
         {
+
             Debug.Log("Z키 누름");
             // 가상환경 경로
             var pythonPath = @"C:\Users\dla12\anaconda3\envs\sesac";
@@ -66,29 +70,41 @@ public class pythonnet : MonoBehaviour
                 dynamic sys = Py.Import("sys"); // 위와 동일
 
                 // 실행할 Python 파일 경로
-                //var python_file_path = @"C:\Users\dla12\source\repos\PythonProject\python_file\pythonnet_test.py";
-                var python_file_path = @"C:\Users\dla12\Documents\Developer\Generative-Conversational-Model-Considering-Age-In-the-Metaverse\python\integrated.py";
+                var python_file_path = @"C:\Users\dla12\source\repos\PythonProject\python_file\pythonnet_test.py";
+                //var python_file_path = @"C:\Users\dla12\Documents\Developer\Generative-Conversational-Model-Considering-Age-In-the-Metaverse\python\integrated.py";
                 sys.path.append(os.path.dirname(os.path.expanduser(python_file_path)));
                 var fromFile = Py.Import(Path.GetFileNameWithoutExtension(python_file_path));
 
                 // pythonnet_test.py 에서 replace 메소드를 호출
                 var a = fromFile.InvokeMethod("unity_run");
-                Debug.Log("파이썬 실행 완료");
-                Debug.Log(a);
-                Debug.Log(a.GetType());
-            }
+                //Debug.Log("파이썬 실행 완료");
 
+                string char_input = a[0].ToString();
+                string char_output = a[1].ToString();
+
+
+                Debug.Log(char_input);
+                Debug.Log(char_output);
+                //Debug.Log(a.GetType());
+                
+            }
             // python 환경을 종료한다.
             PythonEngine.Shutdown();
-            Debug.Log("Python 실행됨");
+            //Debug.Log("Python 실행됨");
+            
         });
+        Debug.Log(char_input);
+        Talk(char_input, char_output);
     }
 
-    void Talk()
+
+
+    void Talk(string char_input, string char_output)
     {
+        Debug.Log("대화창시작");
         AssetDatabase.Refresh();
         //                string comment = ReadTextOneLine("chat_input");
-        string comment = "hi";
+        string comment = char_input;
 
 
         Debug.Log(string.Format("Char Input : {0}", comment));
@@ -103,7 +119,7 @@ public class pythonnet : MonoBehaviour
 
         AssetDatabase.Refresh();
         //string comment2 = ReadTextOneLine("chat_output");
-        string comment2 = "hi2";
+        string comment2 = char_output;
 
         Debug.Log(string.Format("Char output : {0}", comment2));
 
